4fe2199138fe6adbe4943ab5b211183427153a0e
henrynguyen17 <henrynguyen1703@gmail.com>
2022-09-16 22:09:55 +0700

    update frontend v2

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a9f5a9f0d31364f1784a60fbc1873865-968216843.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
