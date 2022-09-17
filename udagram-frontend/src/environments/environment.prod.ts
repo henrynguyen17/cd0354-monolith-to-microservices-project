f06b2099cb9e08ad0415be9ef0911b7e6bb6c067
henrynguyen17 <henrynguyen1703@gmail.com>
2022-09-17 19:57:18 +0700

    reupdate ip fe env

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://ad371f536396840149dccc6d46808c3e-584660488.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a99121db35a18410c999321bf647c44e-1305079551.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
